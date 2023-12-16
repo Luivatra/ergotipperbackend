3b34dc43a69377b030e3e1d80f765af4c3f43486
Robert Pieter van Leeuwen <luivatra@gmail.com>
2023-12-16 13:59:24 +0100

    add message

--- src/main/kotlin/com/ergolui/ergotipperbackend/ErgoUtil.kt
+++ src/main/kotlin/com/ergolui/ergotipperbackend/ErgoUtil.kt
@@ -1,5 +1,6 @@
 package com.ergolui.ergotipperbackend
 
+import javax.annotation.Nonnull
 import org.ergoplatform.appkit.*
 import org.ergoplatform.appkit.Transaction
 import org.ergoplatform.appkit.impl.ErgoTreeContract
@@ -7,8 +8,6 @@ import org.ergoplatform.sdk.ErgoToken
 import org.ergoplatform.sdk.SecretString
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
-import javax.annotation.Nonnull
-
 
 @Component
 class ErgoUtil(private val environment: Environment) {
@@ -21,84 +20,103 @@ class ErgoUtil(private val environment: Environment) {
 
         lateinit var env: Environment
 
-
-        fun validateSeed(seed: String): Boolean
-        {
+        fun validateSeed(seed: String): Boolean {
             try {
                 Mnemonic.checkEnglishMnemonic(seed.split(" "))
-            }
-            catch (e: MnemonicValidationException)
-            {
+            } catch (e: MnemonicValidationException) {
                 return false
             }
             return true
         }
 
         fun sendTx(
-            mnemonic: SecretString,
-            recipient: String,
-            amountToSend: Long,
-            tokensToSend: List<ErgoToken>
+                mnemonic: SecretString,
+                recipient: String,
+                amountToSend: Long,
+                tokensToSend: List<ErgoToken>,
+                message: String?
         ): SendTransactionResult {
             try {
                 val feeAmount = env.getProperty("ergolui.ergotipperbackend.feeamount").toString()
                 val feeAddress = env.getProperty("ergolui.ergotipperbackend.feeaddress").toString()
-                val maxInputBoxes = env.getProperty("ergolui.ergotipperbackend.maxinputboxes").toString()
+                val maxInputBoxes =
+                        env.getProperty("ergolui.ergotipperbackend.maxinputboxes").toString()
                 val nodeUrl = env.getProperty("ergolui.ergotipperbackend.nodeurl").toString()
 
-                val ergoClient = RestApiErgoClient.create(
-                    nodeUrl,
-                    NetworkType.MAINNET,
-                    "",
-                    RestApiErgoClient.defaultMainnetExplorerUrl
-                )
+                val ergoClient =
+                        RestApiErgoClient.create(
+                                nodeUrl,
+                                NetworkType.MAINNET,
+                                "",
+                                RestApiErgoClient.defaultMainnetExplorerUrl
+                        )
                 return ergoClient.execute { ctx: BlockchainContext ->
-                    val proverBuilder = ctx.newProverBuilder()
-                        .withMnemonic(
-                            mnemonic,
-                            SecretString.create(""),
-                            false
-                        ).withEip3Secret(0)
+                    val proverBuilder =
+                            ctx.newProverBuilder()
+                                    .withMnemonic(mnemonic, SecretString.create(""), false)
+                                    .withEip3Secret(0)
                     val prover = proverBuilder.build()
-                    val contract: ErgoContract = ErgoTreeContract(Address.create(recipient).ergoAddress.script(), NetworkType.MAINNET)
-                    val feeContract = ErgoTreeContract(Address.create(feeAddress).ergoAddress.script(), NetworkType.MAINNET)
+                    val contract: ErgoContract =
+                            ErgoTreeContract(
+                                    Address.create(recipient).ergoAddress.script(),
+                                    NetworkType.MAINNET
+                            )
+                    val feeContract =
+                            ErgoTreeContract(
+                                    Address.create(feeAddress).ergoAddress.script(),
+                                    NetworkType.MAINNET
+                            )
 
-                    var boxOperations = BoxOperations.createForSender(prover.eip3Addresses[0], ctx).withAmountToSpend(amountToSend+feeAmount.toLong()).withFeeAmount(1000000)
+                    var boxOperations =
+                            BoxOperations.createForSender(prover.eip3Addresses[0], ctx)
+                                    .withAmountToSpend(amountToSend + feeAmount.toLong())
+                                    .withFeeAmount(1000000)
+                                    .withMessage(message)
                     if (tokensToSend.isNotEmpty())
-                        boxOperations = boxOperations.withTokensToSpend(tokensToSend)
+                            boxOperations = boxOperations.withTokensToSpend(tokensToSend)
 
-                    val txB: UnsignedTransactionBuilder = boxOperations.blockchainContext.newTxBuilder()
+                    val txB: UnsignedTransactionBuilder =
+                            boxOperations.blockchainContext.newTxBuilder()
 
                     val newBoxBuilder = txB.outBoxBuilder().contract(contract).value(amountToSend)
                     val newBox =
-                        if (tokensToSend.isNotEmpty())
-                            newBoxBuilder.tokens(*tokensToSend.toTypedArray()).build()
-                        else
-                            newBoxBuilder.build()
-                    val tipFeeBox = txB.outBoxBuilder().contract(feeContract).value(feeAmount.toLong()).build()
+                            if (tokensToSend.isNotEmpty())
+                                    newBoxBuilder.tokens(*tokensToSend.toTypedArray()).build()
+                            else newBoxBuilder.build()
+                    val tipFeeBox =
+                            txB.outBoxBuilder()
+                                    .contract(feeContract)
+                                    .value(feeAmount.toLong())
+                                    .build()
 
                     txB.addOutputs(newBox, tipFeeBox)
 
                     val boxesLoader = RecordingBoxesLoader().withAllowChainedTx(true)
 
-                    boxOperations.withInputBoxesLoader(boxesLoader)
-                        .withMaxInputBoxesToSelect(maxInputBoxes.toInt())
+                    boxOperations
+                            .withInputBoxesLoader(boxesLoader)
+                            .withMaxInputBoxesToSelect(maxInputBoxes.toInt())
 
                     val boxesToSpend: List<InputBox> = boxOperations.loadTop(0)
 
                     txB.addInputs(*boxesToSpend.toTypedArray())
-                        .fee(boxOperations.feeAmount)
-                        .sendChangeTo(boxOperations.senders[0])
+                            .fee(boxOperations.feeAmount)
+                            .sendChangeTo(boxOperations.senders[0])
 
                     val addedInputs = txB.inputBoxes
                     if (addedInputs.size < maxInputBoxes.toInt()) {
                         val firstSenderErgoTree =
-                            boxOperations.senders.first().toErgoContract().ergoTree
-                        val extraInputBoxes = boxesLoader.allBoxesLoaded.filter {
-                            it.ergoTree == firstSenderErgoTree && !addedInputs.contains(it)
-                        }.take(maxInputBoxes.toInt() - addedInputs.size)
+                                boxOperations.senders.first().toErgoContract().ergoTree
+                        val extraInputBoxes =
+                                boxesLoader
+                                        .allBoxesLoaded
+                                        .filter {
+                                            it.ergoTree == firstSenderErgoTree &&
+                                                    !addedInputs.contains(it)
+                                        }
+                                        .take(maxInputBoxes.toInt() - addedInputs.size)
                         if (extraInputBoxes.isNotEmpty())
-                            txB.addInputs(*extraInputBoxes.toTypedArray())
+                                txB.addInputs(*extraInputBoxes.toTypedArray())
                     }
 
                     val signed = prover.sign(txB.build())
@@ -128,14 +146,15 @@ private class RecordingBoxesLoader : BoxOperations.ExplorerApiWithCheckerLoader(
     }
 
     override fun prepare(
-        @Nonnull ctx: BlockchainContext,
-        addresses: List<Address?>?,
-        grossAmount: Long,
-        @Nonnull tokensToSpend: List<ErgoToken?>
+            @Nonnull ctx: BlockchainContext,
+            addresses: List<Address?>?,
+            grossAmount: Long,
+            @Nonnull tokensToSpend: List<ErgoToken?>
     ) {
         unconfirmedSpentBoxesIds = ArrayList()
         val dataSource = ctx.dataSource
-        val unconfirmedTransactions: MutableList<Transaction>? = dataSource.getUnconfirmedTransactions(0, 1000)
+        val unconfirmedTransactions: MutableList<Transaction>? =
+                dataSource.getUnconfirmedTransactions(0, 1000)
         if (unconfirmedTransactions != null) {
             for (unconfirmedTx in unconfirmedTransactions) {
                 (unconfirmedSpentBoxesIds as ArrayList<String>).addAll(unconfirmedTx.inputBoxesIds)
@@ -152,9 +171,9 @@ private class RecordingBoxesLoader : BoxOperations.ExplorerApiWithCheckerLoader(
     }
 
     override fun loadBoxesPage(
-        ctx: BlockchainContext,
-        sender: Address,
-        page: Int
+            ctx: BlockchainContext,
+            sender: Address,
+            page: Int
     ): MutableList<InputBox> {
         val inputBoxes = super.loadBoxesPage(ctx, sender, page)
 
@@ -173,5 +192,4 @@ private class RecordingBoxesLoader : BoxOperations.ExplorerApiWithCheckerLoader(
         allBoxesLoaded.addAll(inputBoxes)
         return inputBoxes
     }
-
 }
--- src/main/kotlin/com/ergolui/ergotipperbackend/TransactionService.kt
+++ src/main/kotlin/com/ergolui/ergotipperbackend/TransactionService.kt
@@ -1,70 +1,88 @@
 package com.ergolui.ergotipperbackend
 
-import org.springframework.stereotype.Service
-import org.ergoplatform.sdk.ErgoToken
 import kotlin.math.floor
 import kotlin.math.pow
+import org.ergoplatform.sdk.ErgoToken
+import org.springframework.stereotype.Service
 
 @Service
-class TransactionService(val db : TransactionRepository,
-                         val walletService: WalletService,
-                         val tokenDB: TokenRepository,
-                         val accountService: AccountService) {
-    fun findTransactions(fromWallet: String, fromAccount: String): List<Transaction> = db.findTransactions(fromWallet,fromAccount)
+class TransactionService(
+        val db: TransactionRepository,
+        val walletService: WalletService,
+        val tokenDB: TokenRepository,
+        val accountService: AccountService
+) {
+    fun findTransactions(fromWallet: String, fromAccount: String): List<Transaction> =
+            db.findTransactions(fromWallet, fromAccount)
 
-    fun post(transaction: Transaction) : SendTransactionResult {
+    fun post(transaction: Transaction): SendTransactionResult {
         var tokensToSend: List<ErgoToken> = emptyList()
         if (transaction.token.isNotEmpty()) {
             val token = tokenDB.getTokenByName(transaction.token)
-            if(token != null)
-            {
-                tokensToSend = listOf<ErgoToken>(ErgoToken(token.token_id,
-                    floor(transaction.token_amount*10.0.pow(token.decimals.toDouble())).toLong()
-                ))
-            }
-            else
-            {
-                return SendTransactionResult(false,errorMsg = "Unknown token name: " + transaction.token)
+            if (token != null) {
+                tokensToSend =
+                        listOf<ErgoToken>(
+                                ErgoToken(
+                                        token.token_id,
+                                        floor(
+                                                        transaction.token_amount *
+                                                                10.0.pow(token.decimals.toDouble())
+                                                )
+                                                .toLong()
+                                )
+                        )
+            } else {
+                return SendTransactionResult(
+                        false,
+                        errorMsg = "Unknown token name: " + transaction.token
+                )
             }
         }
-        val tx = ErgoUtil.sendTx(walletService.getSeed(transaction.from_wallet),walletService.getAddress(transaction.to_wallet),transaction.amount, tokensToSend)
-        if(tx.success)
-            transaction.succeeded="1"
-        else
-            println(tx.errorMsg)
+        val tx =
+                ErgoUtil.sendTx(
+                        walletService.getSeed(transaction.from_wallet),
+                        walletService.getAddress(transaction.to_wallet),
+                        transaction.amount,
+                        tokensToSend,
+                        transaction.comment
+                )
+        if (tx.success) transaction.succeeded = "1" else println(tx.errorMsg)
         db.save(transaction)
         return tx
     }
 
-    fun tip(transactionLike: TransactionLike): SendTransactionResult
-    {
-        val fromAccount = accountService.findAccount(transactionLike.fromPlatform,transactionLike.fromUsername)
+    fun tip(transactionLike: TransactionLike): SendTransactionResult {
+        val fromAccount =
+                accountService.findAccount(
+                        transactionLike.fromPlatform,
+                        transactionLike.fromUsername
+                )
         if (fromAccount != null) {
-            val toAccount = accountService.findAccount(transactionLike.toPlatform, transactionLike.toUsername)
+            val toAccount =
+                    accountService.findAccount(
+                            transactionLike.toPlatform,
+                            transactionLike.toUsername
+                    )
             if (toAccount != null) {
                 return post(
-                    Transaction(
-                        from_display = transactionLike.fromDisplay,
-                        from_account = fromAccount.id.toString(),
-                        from_wallet = fromAccount.wallet.toString(),
-                        to_display = transactionLike.toDisplay,
-                        to_account = toAccount.id.toString(),
-                        to_wallet = toAccount.wallet.toString(),
-                        amount = transactionLike.amount,
-                        token = transactionLike.tokenName,
-                        token_amount = transactionLike.tokenAmount,
-                        comment = transactionLike.comment
-                    )
+                        Transaction(
+                                from_display = transactionLike.fromDisplay,
+                                from_account = fromAccount.id.toString(),
+                                from_wallet = fromAccount.wallet.toString(),
+                                to_display = transactionLike.toDisplay,
+                                to_account = toAccount.id.toString(),
+                                to_wallet = toAccount.wallet.toString(),
+                                amount = transactionLike.amount,
+                                token = transactionLike.tokenName,
+                                token_amount = transactionLike.tokenAmount,
+                                comment = transactionLike.comment
+                        )
                 )
+            } else {
+                return SendTransactionResult(false, errorMsg = "To account unknown")
             }
-            else
-            {
-                return SendTransactionResult(false,errorMsg = "To account unknown")
-            }
-        }
-        else
-        {
-            return SendTransactionResult(false,errorMsg = "From account unknown")
+        } else {
+            return SendTransactionResult(false, errorMsg = "From account unknown")
         }
     }
 }
